6a9058895a902bb9a61c4aed560a49cc282dd0e1
Burek-man <Alnu07001@utb.vaxjo.se>
2025-01-10 13:01:56 +0100

    script

--- dva.js
+++ dva.js
@@ -1,6 +1,20 @@
 let size=30;
+let explod = false;
 
 function increseheight(){
-    size++;
-    document.getElementById("ballong").style.fontSize = size+"px";
+    size+= 10;
+
+    if (size>= 300 && size < 310){
+        document.getElementById("ballong").innerHTML = "💥";
+    }
+    else if (size >= 309){
+        explod = true;
+    }
+    if (explod) {
+        size = 100;
+    document.getElementById("ballong").innerHTML = "🎈";
+        explod = false;
+    }
+
+    document.getElementById("ballong").style.fontSize = size + "px";
 }
